82e68a401c3f169034f692f063ccdf42dcf78637
stefanfox <stefanovskimedia@gmail.com>
2025-04-28 13:23:30 +0200

    https://github.com/StefanFOX/React-hw.git

--- cas1-hw/src/App.jsx
+++ cas1-hw/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import {City} from './components/city.jsx';
-import { Car } from "./components/car.jsx";
+import {Car} from "./components/car.jsx";
+
+
  export function App(){
 
 
   return 
   <>
-    =
+  
     <h1>This is my city</h1>
     <br/>
     <City/>
